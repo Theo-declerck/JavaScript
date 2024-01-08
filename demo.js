640ceb02bb5ab46aad6bcd09e76ee8fca2a3d018
theo declerck <declercktheo26@gmail.com>
2024-01-08 14:36:34 +0100

    ouais

---
+++ demo.js
@@ -0,0 +1,28 @@
+// DEMO BOUCLE
+
+// for(let i = 0; i<=10; i++){
+//     console.log(i);
+// }
+
+const tab = [0,10,20,30];
+
+// for (let i = 0; i< tab.length; i++){
+//     console.log(tab[i]);
+// }
+
+// for (let value in tab){
+//     console.log(`Indice ${value} : ${tab[value]}`);
+// }
+
+// const perso ={
+//     nom :"toto" ,
+//     age :"10" ,
+// }
+
+// for(let indice in perso){
+//     console.log(`${indice} : ${perso[indice]}`)
+// }
+
+// for(let val of tab){
+//     console.log(`Indice ${val} : ${tab[val]}`)
+// }
---
+++ exo1/script.js
@@ -0,0 +1,7 @@
+const nombre =5;
+ var resultat = 1;
+
+for (let i=1 ; i<=nombre; i++ ){
+    resultat = resultat * i;
+}
+console.log("Factorielle " + nombre +  " :" + "=" + resultat);
